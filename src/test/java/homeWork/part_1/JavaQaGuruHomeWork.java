7ca9ce08cf05a65d1a333dd5dd893f1b9a070619
Agzamov Vladimir <agzamov.qa@yandex.ru>
2026-02-05 17:40:19 +0300

    Add package

---
+++ src/test/java/homeWork/part_1/JavaQaGuruHomeWork.java
@@ -0,0 +1,11 @@
+package homeWork.part_1;
+
+public class JavaQaGuruHomeWork {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
---
+++ src/test/java/lessons/part_1/ExampleJavaClass.java
@@ -0,0 +1,11 @@
+package lessons.part_1;
+
+public class ExampleJavaClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
