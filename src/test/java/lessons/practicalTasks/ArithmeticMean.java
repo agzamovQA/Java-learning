e2097ecc9fa87481bd5c07e3a9fc1afa264f373f
Agzamov Vladimir <agzamov.qa@yandex.ru>
2026-02-06 19:21:04 +0300

    Добавил практическую задачку по среднему арифметическому

---
+++ src/test/java/lessons/practicalTasks/ArithmeticMean.java
@@ -0,0 +1,16 @@
+package lessons.practicalTasks;
+
+public class ArithmeticMean {
+
+    public static void main(String[] args) {
+
+        double firstUserValue = 100;
+        double secondUserValue = 10;
+
+        double result = (firstUserValue + secondUserValue) / 2;
+
+        System.out.println("Средним арифметическим числом между " + firstUserValue + " и " + secondUserValue + " является " + result);
+
+    }
+
+}
